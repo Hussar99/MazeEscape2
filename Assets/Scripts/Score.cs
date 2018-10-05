418359a8c9d48a6e3f5d5b1567a0d73a77b3b104
Hussar99 <43067400+Hussar99@users.noreply.github.com>
2018-10-05 14:52:02 +0100

    Adding Background, Walls, Point and CameraFollow

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    // Public variables visable in Unity
+    public TextMesh displayText;
+
+    // Private variables can't be touched byother scripts
+    private int currentValue = 0;
+
+    // Update both the data value and the visual display
+    public void ChangeValue(int _toChange)
+    {
+        currentValue = currentValue + _toChange;
+        displayText.text = currentValue.ToString();
+    }
+
+    // Reset the score to zero
+    public void ResetScore()
+    {
+        currentValue = 0;
+        displayText.text = currentValue.ToString();
+
+    }
+
+}
---
+++ Assets/Scripts/ScorePickup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePickup : MonoBehaviour {
+
+
+    public Score scoreTracker;
+    public int scoreValue = 1;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+
+    {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //taking the score
+        if (collision.GetComponent<PLayer>())
+
+        {
+            // Updating the score based on how much the object is worth
+            scoreTracker.ChangeValue(scoreValue);
+
+            //getting rid of the object picked
+            Destroy(gameObject);
+        }
+    }
+
+}
